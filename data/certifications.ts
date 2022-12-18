b515d68dd4a0e2c967659db748fa54d26637447f
ysjprojects <85871692+ysjprojects@users.noreply.github.com>
2022-12-18 17:47:45 +0800

    updated certification descriptions

--- data/certifications.ts
+++ data/certifications.ts
@@ -6,7 +6,7 @@ export const CERTIFICATIONS = [
         title: 'Associate Data Analyst',
         org: 'Datacamp',
         url: 'https://www.datacamp.com/certificate/DAA0012302280390',
-        desc: "Course re"
+        desc: "A certification offered by Datacamp that encompasses a broad range of data analytics knowledge from SQL commands to data cleaning, data visualization and statistical analysis with Python libraries (numpy, pandas, seaborn etc.). Certification takes 5 hours to complete and comprises 1 hour of multiple-choice questions and 4 hours of case study"
     },
     {
         id: 2,
@@ -15,7 +15,7 @@ export const CERTIFICATIONS = [
         title: 'Decentralized Finance (DeFi): The Future of Finance',
         org: 'Coursera',
         url: 'https://www.coursera.org/account/accomplishments/specialization/certificate/FSWFS3XEUYNW',
-        desc: "Course re"
+        desc: "A 4-course specialization jointly offered by Coursera and Duke University on Decentralized Finance that covers topics on DeFi primitives and protocols"
     },
     {
         id: 3,
@@ -24,7 +24,7 @@ export const CERTIFICATIONS = [
         title: 'Financial Technology (Fintech) Innovations',
         org: 'Coursera',
         url: 'https://www.coursera.org/account/accomplishments/specialization/certificate/Y5UX9X5SXFAP',
-        desc: "ererer"
+        desc: "A 4-course specialization jointly offered by Coursera and University of Michigan on payments, raising capital, artificial intelligence, as well as other fintech innovations"
     },
     {
         id: 4,
@@ -33,6 +33,6 @@ export const CERTIFICATIONS = [
         title: 'Full-Stack Web Development with React',
         org: 'Coursera',
         url: 'https://www.coursera.org/account/accomplishments/specialization/certificate/KC5C3D847X3J',
-        desc: "ererer"
+        desc: "A 3-course specialization jointly offered by Coursera and The Hong Kong University of Science and Technology that covers Bootstrap and React on the Front-End and ExpressJS On the Back-End"
     }
 ]
